b5aed6e4f9439820e4ef94772507ef68d3e210e0
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-08 15:56:46 -0700

    Fixed file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,11 @@
 /**
  * This program uses puts to print an output
  */
-int main(void) /* This is my main function*/
+int main(void)
 {
+	/**
+	 * Define variable mystr to hold my string
+	 */
 	char mystr[] = "Programming is like building a multilingual puzzle\n";
 
 	puts(mystr);
