053ddbfd77a2501737695d04efee6b63d7beb88a
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-08 16:12:23 -0700

    Fixed file 4-puts.c with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 /**
- * This program uses puts to print an output
+ * main - contains all my code
+ *
+ * Return: signifies end of program
  */
 int main(void)
-	/**
-	 * Blah blah
-	 */
 {
-	/**
-	 * Define variable mystr to hold my string
-	 */
 	char mystr[] = "Programming is like building a multilingual puzzle\n";
 
 	puts(mystr);
 
 	return (0);
-	/**
-	 * Returning zero
-	 */
 }
