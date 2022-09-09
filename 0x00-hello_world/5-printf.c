109e41fb5b2f3f838deb15ceeb9b9fbc5acce11c
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-08 17:00:33 -0700

    Created file 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - contains the main code
+ *
+ * Return: signifies end of code
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+}
