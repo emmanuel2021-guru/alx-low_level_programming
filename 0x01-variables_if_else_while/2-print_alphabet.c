14a964d6526eee5ef3ee2149daf73f222ea2c6d1
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-12 09:09:21 -0700

    Created file 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - holds the main block of code
+ *
+ * Return: Signifies end of code
+ */
+int main(void)
+{
+	int x = 'A';
+
+	while(x <= 'Z')
+	{
+		putchar(tolower(x));
+		x++;
+	}
+	return (0);
+}
