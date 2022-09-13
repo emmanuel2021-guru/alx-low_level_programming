de07159c5ba129da1ad571622694bdd6e2d143db
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-13 16:17:48 -0700

    Created file 9-times_table.c and completed task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int num = 0;
+	int prod;
+
+	while (num <= 9)
+	{
+		_putchar('0');
+
+		int mult = 1;
+
+		while (mult <= 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+			mult++;
+		}
+		num++;
+		_putchar('\n');
+	}
+}
