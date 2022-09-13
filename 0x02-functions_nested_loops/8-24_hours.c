72a8e5ec4076b605d29a6e7ddc705c3935f7b936
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-13 16:04:56 -0700

    Cleaned up code and fixed bugs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,15 +2,16 @@
 
 /**
  * jack_bauer - Prints every minute of Jack Bauer,
- * 		starting from 00:00 to 23:59.
+ * starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
 	int hour = 0;
-	int minute = 0;
 
 	while (hour <= 23)
 	{
+		int minute = 0;
+
 		while (minute <= 59)
 		{
 			_putchar((hour / 10) + '0');
@@ -21,6 +22,7 @@ void jack_bauer(void)
 			_putchar('\n');
 			minute++;
 		}
+
 		hour++;
 	}
 }
