cb89d56f586e64c44dbcde6a1f865b97b368dd54
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-13 09:46:47 -0700

    Created file 1-alphabet.c and completed code

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
