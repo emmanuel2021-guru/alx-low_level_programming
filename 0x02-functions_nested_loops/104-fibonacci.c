b43beecf09e5d3d8cd3ff064c423189854e0f744
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-14 09:57:22 -0700

    Created file 104-fibonacci.c and completed task 15

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count = 0;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
+
+	while (count < 92)
+	{
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		count++;
+	}
+
+	fib1_half1 = fib1 / 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half2 = fib2 % 10000000000;
+
+	count = 93;
+
+	while (count < 99)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
+		if (fib1_half2 + fib2_half2 > 9999999999)
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
+
+		printf("%lu%lu", half1, half2);
+		if (count != 98)
+			printf(", ");
+
+		fib1_half1 = fib2_half1;
+		fib1_half2 = fib2_half2;
+		fib2_half1 = half1;
+		fib2_half2 = half2;
+
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
