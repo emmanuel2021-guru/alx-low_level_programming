c3254c687dd129505303ce200493633b60fd0de2
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-15 11:27:49 -0700

    Created file 5-more_numbers.c and completed task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 0 to 14, 10 times.
+ */
+void more_numbers(void)
+{
+	int new_line, num;
+
+	for (new_line = 0; new_line <= 9; new_line++)
+	{
+		for (num = 0; num <= 14; num++)
+			_putchar(num + '0');
+		_putchar('\n');
+	}
+}
