090b4268876e811dfeae57e964f152d020ab696f
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-29 02:23:14 -0700

    Fixed compiler error in code

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s)
 	int count = 0;
 
 	if (*s != 0)
-		count = ++count + _strlen_recursion(s + 1);
+		count = (count + 1) + _strlen_recursion(s + 1);
 	
 	return (count);
 }
