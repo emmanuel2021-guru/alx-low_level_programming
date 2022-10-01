914b4a102d59b7e7975c14ba48aa6ba59de85223
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-30 18:43:27 -0700

    Created folder and README.md file

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to make change for an amount of money
+ * @argc: Number of arguments supplied
+ * @argv: Array of pointers to arguments
+ *
+ * Return: 0 If number of arguments passed is exactly 1
+ * 1 otherwise.
+ */
+int main(int argc, char *argv[])
+{
+
+}
