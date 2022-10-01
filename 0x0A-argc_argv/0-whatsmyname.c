a3547047a0a6ce1ee56f7d5914124fda4c7d4fd7
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-30 17:09:15 -0700

    Created file and completed task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints program name
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
