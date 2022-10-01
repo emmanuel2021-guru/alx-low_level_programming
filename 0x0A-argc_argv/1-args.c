a6b57dd20b15edd4d01484cae45ae37af1ef21ea
Emmanuel <emmanuellawakin2005@gmail.com>
2022-09-30 17:17:20 -0700

    Fixed code

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  *
  * Return: always 0
  */
-int main(int argc, char __attribute__((__unused__)) argv)
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%s\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
