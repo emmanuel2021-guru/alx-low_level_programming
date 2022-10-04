845fd5b3ef94f1cf9c84e2b44ef0f492ab0397cb
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-04 07:41:55 -0700

    Fixed code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,9 +11,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	c = malloc(sizeof(char) * size);
-	if (c == NULL || size == 0)
+	char *array;
+	int i;
+
+	if (size == 0)
 		return(NULL);
 
-	return (c);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
 }
