c2a724f16b5a37b28f8a0b615b0ceb69f94fa8ea
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-06 06:49:41 -0700

    Created file and completed task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: amount of memory to be allocated.
+ *
+ * Return: a pointer to the allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem_alloc;
+
+	mem_alloc = malloc(b);
+	if (mem_alloc == NULL)
+		exit (98);
+
+	return (mem_alloc);
+}
