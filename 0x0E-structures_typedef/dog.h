46eee5d22be306b4cc29afa0e464b6c37d39f466
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-10 16:01:36 -0700

    Fixed code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,9 +4,14 @@
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
