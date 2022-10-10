355c03e3e9cf712f89159bb84a93ce825bcfe555
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-10 15:54:06 -0700

    Fixed code

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
+/**
+ * struct dog - a new type describing a dog
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog;
+};
