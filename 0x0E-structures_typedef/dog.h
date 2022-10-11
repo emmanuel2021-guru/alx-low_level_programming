0feaba4d8bb160568f8f49effe501ab07398fe6b
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-10 17:03:33 -0700

    Completed tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,3 +15,8 @@ struct dog
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
