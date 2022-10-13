1e708e7e10f114fa7c6417c3202d60fb1a939d52
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-12 18:11:05 -0700

    Completed projects

---
+++ 0x0F-function_pointers/1-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_elem - prints an integer
+ * @elem: the inteer to print.
+ *
+ * Return: Nothing.
+ */
+void print_elem(int elem)
+{
+	
+}
