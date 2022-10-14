29f5a4c7ceca0fbb00dd07f1301954e97027d8a3
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-13 17:17:57 -0700

    Created files and completed tasks 0 - 2

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,9 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
