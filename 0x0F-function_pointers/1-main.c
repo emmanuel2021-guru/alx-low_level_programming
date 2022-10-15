09e1f03c0f6d1a4ecfe6fa4bfe2653864adfb855
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-14 20:05:01 -0700

    Fixed code

--- 0x0F-function_pointers/1-main.c
+++ 0x0F-function_pointers/1-main.c
@@ -9,5 +9,30 @@
  */
 void print_elem(int elem)
 {
+	printf("%d\n", elem);
+}
+
+/**
+ * print_elem_hex - prints an integer, in hexadecimal
+ * @elem: integer to be printed
+ * 
+ * Return: Nothing.
+ */
+void print_elem_hex(int elem)
+{
+	printf("0x%x\n", elem);
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int array[5] = {0, 98, 402, 1024, 4096};
 	
+	array_iterator(array, 5, &print_elem);
+	array_iterator(array, 5, &print_elem_hex);
+	return (0);
 }
