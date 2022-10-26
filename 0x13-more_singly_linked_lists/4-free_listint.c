0ab1ffd282363463724df2e34a758ab8dcc730a9
Emmanuel <emmanuellawakin2005@gmail.com>
2022-10-26 22:48:54 +0100

    Fixed code

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,11 +10,11 @@ void free_listint(listint_t *head)
 {
 	listint_t *tmp;
 
-	tmp = head;
-
-	while (tmp != NULL)
+	while (head != NULL)
 	{
+		tmp = head;
+		head = head->next;
 		free(tmp);
-		tmp = tmp->next;
 	}
+
 }
