b0390a630a301dbd21fdfb7ce5c5ab3b85165d79
Emmanuel <emmanuellawakin2005@gmail.com>
2022-11-05 04:25:33 +0100

    Fixed code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,46 @@
 #include "main.h"
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - creates a buffer
+ * @file: file to create buffer from
+ *
+ * Return: a pointer to an allocated memory space for buffer
+ */
+char *create_buffer(char *file)
+{
+	char *buff;
+
+	buff = malloc(sizeof(char) * 1024);
+	if (buff == NULL)
+	{
+		dprintf(STDERR_FILENO, "Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buff);
+}
+
+/**
+ * close_file - closes a file descriptor
+ * @fd: file descriptor to close
+ *
+ * Return: Nothing
+ */
+void close_file(int fd)
+{
+	int c;
+
+	c = close(fd);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
 /**
  * main - copies the content of a file to another file
  * @ac: number of arguments supplied
@@ -9,7 +50,7 @@
  */
 int main(int ac, char **av)
 {
-	int fd1, fd2, rd_cnt, wrt, clo1, clo2, len = 0, index = 0;
+	int fd1, fd2, rd_cnt, wrt;
 	char *buf;
 
 	if (ac != 3)
@@ -18,15 +59,10 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd1 = open(av[1], O_RDONLY);
-	buf = malloc(sizeof(char) * 1024);
-	if (buf == NULL)
-	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", av[2]);
-	}
+	buf = create_buffer(av[2]);
 	rd_cnt = read(fd1, buf, 1024);
 	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while (rd_cnt > 0)
-	{
+	do {
 		if (rd_cnt == -1 || fd1 == -1)
 		{
 			dprintf(STDERR_FILENO, "Can't read from file %s\n", av[1]);
@@ -40,14 +76,10 @@ int main(int ac, char **av)
 		}
 		rd_cnt = read(fd1, buf, 1024);
 		fd2 = open(av[2], O_WRONLY | O_APPEND);
-	}
+	} while (rd_cnt > 0);
+
 	free(buf);
-	clo1 = close(fd2);
-	clo2 = close(fd1);
-	if (clo2 == -1 || clo1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
+	close_file(fd2);
+	close_file(fd1);
 	return (0);
 }
