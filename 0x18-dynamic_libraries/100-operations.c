886ccd24140bb60130f335591d5dd74ca5db0533
Emmanuel <emmanuellawakin2005@gmail.com>
2022-12-17 04:51:51 +0100

    Created file and completed advanced task 0

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two integers
+ * @a: first integer
+ * @b: second intger
+ *
+ * Return: the difference of the two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the product of the two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of @a divided by @b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the remainder of the division of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the remainder of @a divided by @b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
